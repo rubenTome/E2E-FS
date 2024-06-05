d241d0a23f4f99595ee0bd01ca75ef22620a95f6
rubenTome <rubentome27@gmail.com>
2024-06-05 12:04:47 +0200

    cambios en la script

--- backend_config.py
+++ backend_config.py
@@ -1,23 +1,24 @@
 import keras
 from keras import backend as K, ops, losses
 
-#wrn164 con precision float64 no funciona
-#wrn164 con precision float16 loss infinito
+mnist = {"name": "mnist", "nfeat": 39, "nclass": 10, "batch": 128, "model": "wrn164", "epochs": 60}
+fashion_mnist = {"name": "fashion_mnist", "nfeat": 39, "nclass": 10, "batch": 128, "model": "wrn164","epochs": 60}
+eurosat = {"name": "eurosat", "nfeat": 2048, "nclass": 10, "batch": 128, "model": "wrn164","epochs": 60}
+colorectal_histology = {"name": "colorectal_histology", "nfeat": 33750, "nclass": 8, "batch": 128, "model": "wrn164","epochs": 60}
+cifar10 = {"name": "cifar10", "nfeat": 512, "nclass": 10, "batch": 128, "model": "wrn164","epochs": 60}
+colon = {"name": "colon", "nfeat": 1000, "nclass": 2, "batch": 16, "model": "three_layer_nn","epochs": 60}
+leukemia = {"name": "leukemia", "nfeat": 1785, "nclass": 2, "batch": 16, "model": "three_layer_nn","epochs": 60}
+lung181 = {"name": "lung181", "nfeat": 6266, "nclass": 2, "batch": 16, "model": "three_layer_nn","epochs": 60}
+lymphoma = {"name": "lymphoma", "nfeat": 2013, "nclass": 2, "batch": 16, "model": "three_layer_nn","epochs": 60}
+gisette = {"name": "gisette", "nfeat": 20, "nclass": 2, "batch": 128, "model": "linearSVC","epochs": 100}
+dexter = {"name": "dexter", "nfeat": 9947, "nclass": 2, "batch": 16, "model": "linearSVC","epochs": 150}
+gina = {"name": "gina", "nfeat": 485, "nclass": 2, "batch": 16, "model": "linearSVC","epochs": 150}
+madelon = {"name": "madelon", "nfeat": 30, "nclass": 2, "batch": 16, "model": "linearSVC","epochs": 150}
 
-#POSIBLES VALORES PARA MODEL: "wrn164","three_layer_nn","three_layer_nn_v2"
-mnist = {"name": "mnist", "nfeat": 392, "nclass": 10, "batch": 128, "model": "wrn164"}
-fashion_mnist = {"name": "fashion_mnist", "nfeat": 392, "nclass": 10, "batch": 128, "model": "wrn164"}
-cifar10 = {"name": "cifar10", "nfeat": 512, "nclass": 10, "batch": 128, "model": "wrn164"}
-colon = {"name": "colon", "nfeat": 1000, "nclass": 2, "batch": 16, "model": "three_layer_nn_v2"}
-leukemia = {"name": "leukemia", "nfeat": 1785, "nclass": 2, "batch": 16, "model": "three_layer_nn"}
-lung181 = {"name": "lung181", "nfeat": 6266, "nclass": 2, "batch": 16, "model": "three_layer_nn"}
-lymphoma = {"name": "lymphoma", "nfeat": 2013, "nclass": 2, "batch": 16, "model": "three_layer_nn"}
-
-precision = "float16"
+precision = "float32"
 script = "/home/lidia/Documents/ruben/E2E-FS/example.py"
-selected_dataset = mnist
+selected_dataset = colon
 loss = losses.CategoricalCrossentropy(from_logits=False)
-epochs = 60
 outputFileName = "emissions_" + selected_dataset["name"] + "_" + precision + ".csv"
 
 print('using precision:', precision, 'ok')
--- example.py
+++ example.py
@@ -2,33 +2,150 @@ from codecarbon import EmissionsTracker
 from backend_config import outputFileName
 tracker = EmissionsTracker(log_level="warning", output_file=outputFileName)
 tracker.start()
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import time
+import numpy as np
+import pandas as pd
+import keras
 from keras.callbacks import LearningRateScheduler
 from keras.utils import to_categorical
-from keras import optimizers
-from e2efs import models
-import numpy as np
-from keras import backend as K
-from backend_config import bcknd, ops, selected_dataset, loss, epochs
-import pandas as pd
-from src.wrn.network_models import three_layer_nn_v2, wrn164, three_layer_nn
-from dataset_reader import colon, leukemia, lung181, lymphoma
 from keras.datasets import mnist, cifar10, fashion_mnist
-import keras
+from keras import optimizers, backend as K, callbacks
+from e2efs import models, e2efs_layers_tf216 as e2efs_layers, callbacks as clbks, optimizers_tf216
+from backend_config import bcknd, ops, selected_dataset, loss
+from src.wrn.network_models import wrn164, three_layer_nn, three_layer_nn_v2
+from dataset_reader import colon, leukemia, lung181, lymphoma, gisette, dexter, gina, madelon
+from src.svc.models import LinearSVC
+import tensorflow_datasets as tfds
+#import warnings
+#warnings.filterwarnings("error", category=RuntimeWarning)
 
 ops.cast_to_floatx = lambda x: ops.cast(x, keras.config.floatx())
 K.backend = bcknd
 
+#params for fs_challenge datasets
+mu = 100
+kernel = 'linear'
+reps = 1
+verbose = 0
+loss_function = 'square_hinge'
+optimizer_class = optimizers_tf216.E2EFS_Adam
+initial_lr = .01
+
+def scheduler_ft(epoch):
+    if epoch < 20:
+        return .1
+    elif epoch < 40:
+        return .02
+    elif epoch < 50:
+        return .004
+    else:
+        return .0008
+    
+def scheduler():
+    def sch(epoch):
+        if epoch < 50:
+            return initial_lr
+        elif epoch < 100:
+            return .2 * initial_lr
+        else:
+            return .04 * initial_lr
+
+    return sch
+
+#model creation for linearSVC
+def train_Keras(train_X, train_y, test_X, test_y, normalization_func, kwargs, e2efs_class=None, n_features=None, epochs=150):
+    normalization = normalization_func()
+    num_classes = train_y.shape[-1]
+
+    norm_train_X = normalization.fit_transform(train_X)
+    norm_test_X = normalization.transform(test_X)
+
+    batch_size = max(2, len(train_X) // 50)
+    class_weight = train_y.shape[0] / np.sum(train_y, axis=0)
+    class_weight = num_classes * class_weight / class_weight.sum()
+    sample_weight = None
+    print('mu :', kwargs['mu'], ', batch_size :', batch_size, ', n_feats :', n_features)
+    print('reps : ', reps, ', weights : ', class_weight)
+    if num_classes == 2:
+        sample_weight = np.zeros((len(norm_train_X),))
+        sample_weight[train_y[:, 1] == 1] = class_weight[1]
+        sample_weight[train_y[:, 1] == 0] = class_weight[0]
+        class_weight = None
+
+    svc_model = LinearSVC(nfeatures=norm_train_X.shape[1:], **kwargs)
+    svc_model.create_keras_model(nclasses=num_classes)
+
+    model_clbks = [
+        callbacks.LearningRateScheduler(scheduler()),
+    ]
+
+    fs_callbacks = []
+
+    if e2efs_class is not None:
+        classifier = svc_model.model
+        e2efs_layer = e2efs_class(n_features, input_shape=norm_train_X.shape[1:])
+        model = e2efs_layer.add_to_model(classifier, input_shape=norm_train_X.shape[1:])
+        fs_callbacks.append(
+            clbks.E2EFSCallback(verbose=verbose)
+        )
+    else:
+        model = svc_model.model
+        e2efs_layer = None
+
+    optimizer = optimizer_class(e2efs_layer )
+
+    model.compile(
+        loss=LinearSVC.loss_function(loss_function, class_weight),
+        optimizer=optimizer,
+        metrics=[LinearSVC.accuracy]
+    )
+
+    if e2efs_class is not None:
+        model.fs_layer = e2efs_layer
+        model.heatmap = e2efs_layer.moving_heatmap
+
+        start_time = time.process_time()
+        model.fit(
+            norm_train_X, train_y, batch_size=batch_size,
+            epochs=200000,
+            callbacks=fs_callbacks,
+            validation_data=(norm_test_X, test_y),
+            class_weight=class_weight,
+            sample_weight=sample_weight,
+            verbose=verbose
+        )
+        model.fs_time = time.process_time() - start_time
+
+    model.fit(
+        norm_train_X, train_y, batch_size=batch_size,
+        epochs=epochs,
+        callbacks=model_clbks,
+        validation_data=(norm_test_X, test_y),
+        class_weight=class_weight,
+        sample_weight=sample_weight,
+        verbose=verbose
+    )
+
+    model.normalization = normalization
+
+    return model
+
 print("model function:", selected_dataset["model"])
 if selected_dataset["model"] == "three_layer_nn":
     model_fun = three_layer_nn
-elif selected_dataset["model"] == "wrn164":
-    model_fun = wrn164
 elif selected_dataset["model"] == "three_layer_nn_v2":
     model_fun = three_layer_nn_v2
+elif selected_dataset["model"] == "wrn164":
+    model_fun = wrn164
+elif selected_dataset["model"] == "linearSVC":
+    model_fun = LinearSVC
 else:
     raise Exception("invalid model function")
 
 microarr = False
+tf_ds = False
 print("used dataset:", selected_dataset["name"])
 if selected_dataset["name"] == "mnist":
     dataset = mnist.load_data
@@ -48,6 +165,24 @@ elif selected_dataset["name"] == "lung181":
 elif selected_dataset["name"] == "lymphoma":
     microarr = True
     dataset = lymphoma.load_dataset
+elif selected_dataset["name"] == "gisette":
+    microarr = True #TEMPORAL
+    dataset = gisette.load_dataset
+    normalization_func = gisette.Normalize
+elif selected_dataset["name"] == "dexter":
+    microarr = True
+    dataset = dexter.load_dataset
+    normalization_func = dexter.Normalize
+elif selected_dataset["name"] == "gina":
+    microarr = True
+    dataset = gina.load_dataset
+    normalization_func = gina.Normalize
+elif selected_dataset["name"] == "madelon":
+    microarr = True
+    dataset = madelon.load_dataset
+    normalization_func = madelon.Normalize
+elif selected_dataset["name"] == "eurosat" or selected_dataset["name"] == "colorectal_histology":#faltan añadir mas ds de tensorflow
+    tf_ds = True
 else:
     raise Exception("Invalid dataset", selected_dataset["name"])
 
@@ -55,61 +190,63 @@ if __name__ == '__main__':
 
     ## LOAD DATA
     # if temporal, para diferenciar microarray de los demas conjuntos de datos
-    if not microarr:
+    if microarr:
+        ds = dataset()
+        data = ds["raw"]["data"]
+        label = ds["raw"]["label"]
+        #dividimos el dataset para crear train (2/3) y test (1/3)
+        x_train = data[:2 * int(len(data) / 3)]
+        x_test = data[2 * int(len(data) / 3):]
+        mean_data = x_train.mean(axis=0)
+        std_data = x_train.std(axis=0) + 1e-8
+        x_train = (x_train - mean_data) / std_data
+        x_test = (x_test - mean_data) / std_data
+        y_train = to_categorical(label[:2 * int(len(label) / 3)], num_classes=selected_dataset["nclass"])
+        y_test = to_categorical(label[2 * int(len(label) / 3):], num_classes=selected_dataset["nclass"])
+    elif tf_ds:
+        x_train = []
+        y_train = []
+        x_test = []
+        y_test = []
+        if selected_dataset["name"] == "eurosat" or selected_dataset["name"] == "colorectal_histology":
+            train_ds, test_ds = tfds.load(selected_dataset["name"], as_supervised=True, split=["train[:70%]", "train[70%:]"])
+        else:
+            train_ds, test_ds = tfds.load(selected_dataset["name"], as_supervised=True, split=["train", "test"])
+        for image, label in tfds.as_numpy(train_ds):
+            x_train.append(image)
+            y_train.append(label)
+        for image, label in tfds.as_numpy(test_ds):
+            x_test.append(image)
+            y_test.append(label)
+        x_train = np.array(x_train)
+        x_test = np.array(x_test)
+        y_train = to_categorical(np.array(y_train).astype(float))
+        y_test = to_categorical(np.array(y_test).astype(float))
+    else:
         (x_train, y_train), (x_test, y_test) = dataset()
         if dataset == mnist.load_data or dataset == fashion_mnist.load_data:
             x_train = np.expand_dims(x_train, axis=-1)
             x_test = np.expand_dims(x_test, axis=-1)
         y_train = to_categorical(y_train, num_classes=selected_dataset["nclass"])
         y_test = to_categorical(y_test, num_classes=selected_dataset["nclass"])
-    else:
-        ds = dataset()
-        data = ds["raw"]["data"]
-        label = ds['raw']["label"]
-        #dividimos el dataset para crear train (2/3) y test (1/3)
-        x_train = data[:2 * int(len(data) / 3)]
-        x_test = data[2 * int(len(data) / 3):]
-        mean_data = x_train.mean(axis=0)
-        std_data = x_train.std(axis=0) + 1e-8
-        x_train = (x_train - mean_data)/std_data
-        x_test = (x_test - mean_data)/std_data
-        y_train = to_categorical(label[:2 * int(len(label) / 3)], num_classes=selected_dataset["nclass"])
-        y_test = to_categorical(label[2 * int(len(label) / 3):], num_classes=selected_dataset["nclass"])
 
     ## LOAD MODEL AND COMPILE IT (NEVER FORGET TO COMPILE!)
-    model = model_fun(input_shape=x_train.shape[1:], nclasses=selected_dataset["nclass"], regularization=5e-4)
-    model.compile(optimizer=optimizers.SGD(), metrics=['acc'], loss=loss)
-    # model.fit(
-    #    x_train, y_train, batch_size=batch_size, validation_data=(x_test, y_test), verbose=2
-    # )
+    if selected_dataset["model"] == "linearSVC":
+        model_kwargs = {'mu': mu / len(x_train), 'kernel': kernel, 'degree': 3}
+        model = train_Keras(x_train, y_train, x_test, y_test, normalization_func, model_kwargs, 
+                            e2efs_class=e2efs_layers.E2EFSSoft, n_features=selected_dataset["nfeat"])
+    else:
+        model = model_fun(input_shape=x_train.shape[1:], nclasses=selected_dataset["nclass"], regularization=5e-4)
+        model.compile(optimizer=optimizers.SGD(), metrics=['acc'], loss=loss)
 
     ## LOAD E2EFS AND RUN IT
     fs_class = models.E2EFSSoft(n_features_to_select=selected_dataset["nfeat"]).attach(model).fit(
         x_train, y_train, batch_size=selected_dataset["batch"], validation_data=(x_test, y_test), verbose=2
     )
-    ## OPTIONAL: LOAD E2EFS AND RUN IT
-    # fs_class = models.E2EFS(n_features_to_select=39).attach(model).fit(
-    #     x_train, y_train, batch_size=128, validation_data=(x_test, y_test), verbose=2
-    # )
-
-    ## OPTIONAL: LOAD E2EFSRanking AND RUN IT (do not use fine tuning with this model, only get_ranking)
-    # fs_class = models.E2EFSRanking().attach(model).fit(
-    #     x_train, y_train, batch_size=128, validation_data=(x_test, y_test), verbose=2
-    # )
 
     ## FINE TUNING
-    def scheduler(epoch):
-        if epoch < 20:
-            return .1
-        elif epoch < 40:
-            return .02
-        elif epoch < 50:
-            return .004
-        else:
-            return .0008
-
-    fs_class.fine_tuning(x_train, y_train, epochs=epochs, batch_size=selected_dataset["batch"], validation_data=(x_test, y_test),
-                         callbacks=[LearningRateScheduler(scheduler)], verbose=2)
+    fs_class.fine_tuning(x_train, y_train, epochs=selected_dataset["epochs"], batch_size=selected_dataset["batch"], validation_data=(x_test, y_test),
+                         callbacks=[LearningRateScheduler(scheduler_ft)], verbose=2)
     print('FEATURE_RANKING :', fs_class.get_ranking())
     acc = fs_class.get_model().evaluate(x_test, y_test, batch_size=selected_dataset["batch"])[-1]
     print('ACCURACY : ', acc)
