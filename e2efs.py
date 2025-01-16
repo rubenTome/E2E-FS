fixing 16-bit precision

e2efs.py
@@ -7,16 +7,11 @@ import lightning as pl
 from copy import deepcopy
 from src.dataloaders import FastTensorDataLoader
 from src.callbacks import MyEarlyStopping
-import sys
 
-if sys.argv[1] == "16":
-    torch.set_float32_matmul_precision("high")
-if sys.argv[1] == "64":
-    torch.set_default_dtype(torch.float64)
 
 class E2EFSBase:
 
-    def __init__(self, n_features_to_select, feature_importance, wait, precision, network=None, mask_name='E2EFSSoftMask',
+    def __init__(self, n_features_to_select, feature_importance=.1, wait=10, precision='32', network=None, mask_name='E2EFSSoftMask',
                  balanced=True, regularization='default'):
         self.n_features_to_select = n_features_to_select
         self.feature_importance = feature_importance
@@ -30,7 +25,8 @@ class E2EFSBase:
         self.regularization = regularization
 
     def __build_mask__(self, input_shape):
-        return getattr(layers, self.mask_name)(input_shape=input_shape, n_features_to_select=self.n_features_to_select, feature_importance=self.feature_importance)
+        return getattr(layers, self.mask_name)(input_shape=input_shape, n_features_to_select=self.n_features_to_select,
+                                               feature_importance=self.feature_importance, epsilon=1e-3 if '16' in self.precision else 1e-8)
 
     def __build_model__(self, X, y):
         input_shape = X.shape[1:]
@@ -52,7 +48,7 @@ class E2EFSBase:
             reg = self.__select_default_regularization(architecture, X) if self.regularization == 'default' else self.regularization
             default_model = default_models.DefaultRegressor if self.task == 'regression' else default_models.DefaultClassifier
             network_model = default_model(input_shape=input_shape, output_size=output_size, architecture=architecture, regularization=reg)
-        return E2EFSModel(network=network_model, e2efs_layer=mask)
+        return E2EFSModel(network=network_model, e2efs_layer=mask, epsilon=1e-3 if '16' in self.precision else 1e-8)
 
     def __select_default_architecture(self, X, y):
         nsamples, nfeats = len(X), np.prod(X.shape[1:])

src/callbacks.py
@@ -1,11 +1,5 @@
 import lightning as pl
-import torch
-import sys
-
-if sys.argv[1] == "16":
-    torch.set_float32_matmul_precision("high")
-if sys.argv[1] == "64":
-    torch.set_default_dtype(torch.float64)
+import math
 
 
 class MyEarlyStopping(pl.pytorch.callbacks.EarlyStopping):
@@ -29,7 +23,7 @@ class MyEarlyStopping(pl.pytorch.callbacks.EarlyStopping):
         else:
             self.wait_counter = 0
         self.nfeats = nfeats
-        alpha = pl_module.e2efs_layer.moving_factor
-        if nfeats < self.stopping_threshold or self.wait_counter >= self.wait:
+        alpha = pl_module.e2efs_layer.get_factor().item()
+        if math.isclose(alpha, self.feature_importance, abs_tol=1e-3) and (nfeats < self.stopping_threshold or self.wait_counter >= self.wait):
             trainer.should_stop = True
         print('\tnfeats {} (threshold {}) alpha {:.4f} (threshold {})'.format(nfeats, self.stopping_threshold, alpha, self.feature_importance))

src/e2efs_models.py
@@ -3,18 +3,12 @@ from typing import Optional, Any
 import lightning as pl
 import torch
 from .layers import E2EFSMaskBase
-import sys
-
-if sys.argv[1] == "16":
-    torch.set_float32_matmul_precision("high")
-if sys.argv[1] == "64":
-    torch.set_default_dtype(torch.float64)
 
 
 class E2EFSModel(pl.LightningModule):
 
     def __init__(self, network: pl.LightningModule, e2efs_layer: E2EFSMaskBase,
-                 epsilon: float = 1e-8, threshold: float = 1.):
+                 epsilon: float = 1e-3, threshold: float = 1.):
         super(E2EFSModel, self).__init__()
         self.network = network
         self.e2efs_layer = e2efs_layer
@@ -30,7 +24,7 @@ class E2EFSModel(pl.LightningModule):
         return output
 
     def __combine_grads(self, network_grad, e2efs_grad):
-        alpha = self.e2efs_layer.moving_factor
+        alpha = self.e2efs_layer.get_factor()
         network_grad_norm = torch.norm(network_grad) + self.epsilon
         e2efs_grad_normalized = e2efs_grad / (torch.norm(e2efs_grad) + self.epsilon)
         network_grad_normalized = network_grad / network_grad_norm
@@ -59,7 +53,7 @@ class E2EFSModel(pl.LightningModule):
             penalty_grad = self.e2efs_layer.kernel.grad
             self.e2efs_layer.kernel.grad = self.__combine_grads(network_grad, penalty_grad)
             e2efs_opt.step()
-            self.log_dict({"alpha": self.e2efs_layer.moving_factor, "penalty": penalty}, prog_bar=True)
+            self.log_dict({"alpha": self.e2efs_layer.get_factor(), "penalty": penalty}, prog_bar=True)
         network_opt.step()
         self.e2efs_layer.kernel_constraint()
         # loss = F.cross_entropy(y_hat, y) # - 1e-4*torch.sum(s * torch.log(s_clip))
@@ -84,7 +78,7 @@ class E2EFSModel(pl.LightningModule):
 
     def configure_optimizers(self):
         network_opt = self.network.configure_optimizers()
-        e2efs_opt = torch.optim.Adam(self.e2efs_layer.parameters(), lr=1e-3 if self.fitted else 1e-3)
+        e2efs_opt = torch.optim.Adam(self.e2efs_layer.parameters(), lr=1e-3 if self.fitted else 1e-3, eps=self.epsilon)
         # e2efs_opt = torch.optim.SGD(self.e2efs_layer.parameters(), lr=.01)
         callbacks = []
         if self.fitted:

src/layers.py
@@ -1,13 +1,6 @@
 import torch
 from torch import nn
 import numpy as np
-import sys
-
-if sys.argv[1] == "16":
-    torch.set_float32_matmul_precision("high")
-if sys.argv[1] == "64":
-    torch.set_default_dtype(torch.float64)
-
 
 
 class E2EFSMaskBase(nn.Module):
@@ -25,6 +18,7 @@ class E2EFSMaskBase(nn.Module):
         self.register_buffer('moving_units', torch.tensor(n_features_to_select, **factory_kwargs))
         self.register_buffer('moving_T', torch.tensor(0., **factory_kwargs))
         self.register_buffer('moving_factor', torch.tensor(0., **factory_kwargs))
+        self.register_buffer('moving_factor_aux', torch.tensor(0., **factory_kwargs))
         self.heatmap_momentum = heatmap_momentum
         self.n_features_to_select = n_features_to_select
         self.feature_importance = feature_importance
@@ -37,11 +31,19 @@ class E2EFSMaskBase(nn.Module):
         self.heatmap = self.heatmap_momentum * self.heatmap + (1. - self.heatmap_momentum) * torch.sign(self.kernel_activation())
         self.moving_T = self.moving_T + 1
         if (self.moving_factor < self.feature_importance):
+            moving_factor = self.moving_factor
             self.moving_factor = torch.where(
                 torch.less(self.moving_T, self.warmup_T),
                 self.start_alpha,
-                (self.start_alpha + (1. - self.start_alpha) * (self.moving_T - self.warmup_T) / self.T).clamp(max=self.alpha_M)
+                self.moving_factor + self.feature_importance / self.T
+                # (self.start_alpha + (1. - self.start_alpha) * self.feature_importance * (self.moving_T - self.warmup_T) / self.T)
             )
+            if moving_factor == self.moving_factor:
+                self.moving_factor_aux = self.moving_factor_aux + self.moving_factor
+                self.moving_factor *= 0.
+
+    def get_factor(self):
+        return (self.moving_factor + self.moving_factor_aux).clamp(max=self.feature_importance)
 
     def get_penalty(self):
         x = self.kernel
@@ -79,7 +81,7 @@ class E2EFSSoftMask(E2EFSMaskBase):
 
     def __init__(self, input_shape, n_features_to_select, feature_importance,
                  decay_factor=.75,
-                 T=20000,
+                 T=2000,
                  warmup_T=2000,
                  start_alpha=.0,
                  alpha_N=.99,
@@ -104,7 +106,7 @@ class E2EFSSoftMask(E2EFSMaskBase):
     def update_buffers(self):
         super(E2EFSSoftMask, self).update_buffers()
         self.moving_decay = torch.where(
-            torch.less(self.moving_factor, self.alpha_M),
+            torch.less(self.get_factor(), self.alpha_M),
             self.moving_decay,
             (self.moving_decay + self.epsilon).clamp(min=.75)
         )
@@ -118,7 +120,7 @@ class E2EFSMask(E2EFSSoftMask):
         super(E2EFSMask, self).__init__(input_shape=input_shape,
                                     n_features_to_select=n_features_to_select,
                                     decay_factor=0.,
-                                    T=10000, device=device, dtype=dtype)
+                                    T=1000, device=device, dtype=dtype)
 
 
 class E2EFSRamkingMask(E2EFSSoftMask):
@@ -130,7 +132,7 @@ class E2EFSRamkingMask(E2EFSSoftMask):
         super(E2EFSRamkingMask, self).__init__(input_shape=input_shape,
                                     n_features_to_select=n_features_to_select,
                                     decay_factor=0.,
-                                    T=20000, device=device, dtype=dtype)
+                                    T=2000, device=device, dtype=dtype)
         self.register_buffer('speedup', torch.tensor(speedup, device=device, dtype=dtype))
 
     def update_buffers(self):